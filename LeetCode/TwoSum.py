77c0e5c3165ea57f00a681685506ae6acb3dc5ed
Hithru <hithrudealwis@gmail.com>
2022-01-23 13:32:53 +0530

    LeetCode: Two Sum - Easy

---
+++ LeetCode/TwoSum.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int):
+        dict = {}
+        for i in range(len(nums)):
+            difference = target - nums[i]
+            if nums[i] in dict.keys():
+                return [dict[nums[i]], i]
+            else:
+                dict[target - nums[i]] = i
+        return None
