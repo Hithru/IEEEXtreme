c16c09990263a526a3fe1196ef0853101a5352c0
Hithru <hithrudealwis@gmail.com>
2022-08-09 21:47:21 +0530

    HackerRank: Encryption

---
+++ HackerRank/Encryption.py
@@ -0,0 +1,22 @@
+# Challenge Name: Encryption
+# Difficulty: Easy
+
+def encryption(s):
+    modified_string = s.replace(" ", "")
+    length = len(modified_string)
+    sqrt_length = length ** 0.5
+    column = int(sqrt_length)
+    if sqrt_length > int(sqrt_length):
+        column += 1
+
+    answer = ["" for i in range(column)]
+
+    for i in range(len(modified_string)):
+        answer[i % column] += modified_string[i]
+
+    return " ".join(answer)
+
+
+print(encryption('haveaniceday'))
+print(encryption('feedthedog'))
+print(encryption('chillout'))
