f99027748c84e1affefa6d0a6be8899dfb1e1d8e
Hithru <hithrudealwis@gmail.com>
2022-08-19 12:59:52 +0530

    HackerRank: ProblemSolving :- Two Characters

--- HackerRank/ProblemSolving/panagrems.py
+++ HackerRank/ProblemSolving/panagrems.py
@@ -1,12 +1,15 @@
-# Challenge Name: Panagrams
+# Challenge Name: Two Characters
 # Difficulty: Easy
 
-def pangrams(sentence):
-    sentence = sentence.lower().replace(' ', '')
-    set_characters = set(list(sentence))
+def two_characters(string: str):
+    unique_string_characters = list(set(string))
+    max_alternative_count = 0
+    for x in range(len(unique_string_characters)):
+        for y in range(x + 1, len(unique_string_characters)):
+            case = [z for z in string if z == unique_string_characters[x] or z == unique_string_characters[y]]
+            if len(set(case[::2])) == 1 and len(set(case[1::2])) == 1:
+                max_alternative_count = max(len(case), max_alternative_count)
+    return max_alternative_count
 
-    return "pangram" if len(set_characters) == 26 else "not pangram"
 
-
-print(pangrams('We promptly judged antique ivory buckles for the next prize'))
-print(pangrams('We promptly judged antique ivory buckles for the prize'))
+print(two_characters('beabeefeab'))
