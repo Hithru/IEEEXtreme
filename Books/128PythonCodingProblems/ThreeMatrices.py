2dfe2993f44a4e30f3b966c4d0c800ef5e84bfe2
Hithru <hithrudealwis@gmail.com>
2022-08-31 21:42:40 +0530

    Book: 128 Python problems: Three matrics check AB = C using freivalds

---
+++ Books/128PythonCodingProblems/ThreeMatrices.py
@@ -0,0 +1,38 @@
+from random import randint
+from sys import stdin
+
+
+def readint():
+    return int(stdin.readline())
+
+
+def readarray(typ):
+    return list(map(typ, stdin.readline().split()))
+
+
+def readmatrix(n):
+    M = []
+    for _ in range(n):
+        row = readarray(int)
+        assert len(row) == n
+        M.append(row)
+    return M
+
+
+def mult(M, v):
+    n = len(M)
+    return [sum(M[i][j] * v[j] for j in range(n)) for i in range(n)]
+
+
+def freivalds(A, B, C):
+    n = len(A)
+    x = [randint(0, 1000000) for j in range(n)]
+    return mult(A, mult(B, x)) == mult(C, x)
+
+
+if __name__ == "__main__":
+    n = readint()
+    A = readmatrix(n)
+    B = readmatrix(n)
+    C = readmatrix(n)
+    print(freivalds(A, B, C))
