d7020f37c9195da4fac9fcd8d74d39fea5431758
Hithru <hithrudealwis@gmail.com>
2022-09-06 00:59:59 +0530

    HackerRank: ProblemSolving :- Common Child: Update to use List

--- HackerRank/ProblemSolving/CommonChild.py
+++ HackerRank/ProblemSolving/CommonChild.py
@@ -2,20 +2,17 @@
 # Difficulty: Intermediate
 
 def common_child(str1: str, str2: str):
-    common_subsequence_length = {}
+    common_subsequence_length = [[0] * (len(str2) + 1) for _ in range(len(str1) + 1)]
     for i in range(len(str1)):
         for j in range(len(str2)):
             if str1[i] == str2[j]:
-                common_subsequence_length.setdefault((i, j), 0)
-                common_subsequence_length[(i + 1, j + 1)] = common_subsequence_length[(i, j)] + 1
+                common_subsequence_length[i + 1][j + 1] = common_subsequence_length[i][j] + 1
 
             else:
-                common_subsequence_length.setdefault((i, j + 1), 0)
-                common_subsequence_length.setdefault((i + 1, j), 0)
-                common_subsequence_length[(i + 1, j + 1)] = max(
-                    common_subsequence_length[(i, j + 1)], common_subsequence_length[(i + 1, j)])
+                common_subsequence_length[i + 1][j + 1] = max(
+                    common_subsequence_length[i][j + 1], common_subsequence_length[i + 1][j])
 
-    return common_subsequence_length[(len(str1), len(str2))]
+    return common_subsequence_length[len(str1)][len(str2)]
 
 
 print(common_child("hello", "world"))
