591c91945a33b663e3d500013f4d49bfa03362fb
Hithru <hithrudealwis@gmail.com>
2022-09-18 17:57:33 +0530

    HackerRank: ProblemSolving :- Smart Number

---
+++ HackerRank/ProblemSolving/SmartNumber.py
@@ -0,0 +1,21 @@
+# Challenge Name: Smart Number
+# Difficulty: Easy
+
+
+import math
+
+
+def is_smart_number(num):
+    val = int(math.sqrt(num))
+    if num / val == val:
+        return True
+    return False
+
+
+for _ in range(int(input())):
+    num = int(input())
+    ans = is_smart_number(num)
+    if ans:
+        print("YES")
+    else:
+        print("NO")
