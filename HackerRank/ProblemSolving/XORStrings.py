68dafb7ef5104ea5349afc9d3f929cd983f424d1
Hithru <hithrudealwis@gmail.com>
2022-09-23 08:42:56 +0530

    HackerRank: ProblemSolving :- XOR Strings

---
+++ HackerRank/ProblemSolving/XORStrings.py
@@ -0,0 +1,17 @@
+# Challenge Name: XOR Strings
+# Difficulty: Easy
+
+def strings_xor(s, t):
+    res = ""
+    for i in range(len(s)):
+        if s[i] == t[i]:
+            res += '0'
+        else:
+            res += '1'
+
+    return res
+
+
+s = input()
+t = input()
+print(strings_xor(s, t))
