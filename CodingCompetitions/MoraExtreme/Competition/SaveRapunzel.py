a12ff55958fba1431fc21489326182e7fb639f15
Hithru <hithrudealwis@gmail.com>
2022-10-20 18:36:09 +0530

    Competition: MoraXtreme7 : Save Rapunzel 1

--- CodingCompetitions/MoraExtreme/Competition/SaveRapunzel.py
+++ CodingCompetitions/MoraExtreme/Competition/SaveRapunzel.py
@@ -1,58 +1,65 @@
-def get_number(k, tile):
-    scope_tile = tile
-    moves = 0
-    new_line = lines[k]
+import sys
+
+sys.setrecursionlimit(1500)
+
+
+def get_number_end(line, tile, end_right):
+    new_line = lines[line]
     max_number = max(new_line)
     min_number = min(new_line)
-    if max_number > scope_tile:
-
-        if min_number >= scope_tile:
-            moves += (max_number - scope_tile)
-            scope_tile = max_number
+    if end_right:
+        if min_number >= tile:
+            moves = max_number - tile
         else:
-            number_of_moves_to_right = max_number - scope_tile
-            number_of_moves_to_left = scope_tile - min_number
-            if number_of_moves_to_left > number_of_moves_to_right:
-                moves += (number_of_moves_to_right + max_number - min_number)
-                scope_tile = min_number
-            elif number_of_moves_to_left < number_of_moves_to_right:
-                moves += (number_of_moves_to_left + max_number - min_number)
-                scope_tile = max_number
-            else:
-                go_left = True
-                if k != n - 2:
-                    next_line = lines[k + 1]
-                    next_max_number = max(next_line)
-                    next_min_number = min(next_line)
-                    if get_number(k + 1, max_number) > get_number(k + 1, min_number):
-                        go_left = False
-                if go_left:
-                    moves += (number_of_moves_to_left + max_number - min_number)
-                    scope_tile = max_number
-                else:
-                    moves += (number_of_moves_to_right + max_number - min_number)
-                    scope_tile = min_number
-
+            moves = tile - min_number + max_number - min_number
     else:
-        moves += (scope_tile - min_number)
-        scope_tile = min_number
-    return moves, scope_tile
+        if max_number <= tile:
+            moves = tile - min_number
+        else:
+            moves = max_number - min_number + max_number - tile
+    return moves
+
+
+def choose_best(n, k, tile):
+    if (k, tile) in dynamic_list:
+        return dynamic_list[(k, tile)]
+    elif k == n - 1:
+        dynamic_list[(k, tile)] = min(get_number_end(k, tile, True), get_number_end(k, tile, False))
+        return dynamic_list[(k, tile)]
+    else:
+        new_line = lines[k]
+        max_number = max(new_line)
+        min_number = min(new_line)
+        end_right = get_number_end(k, tile, True)
+        end_left = get_number_end(k, tile, False)
+        answer = min(end_right + choose_best(n, k + 1, max_number), end_left + choose_best(n, k + 1, min_number))
+        dynamic_list[(k, tile)] = answer
+        return answer
 
 
 test_cases = int(input())
 for i in range(test_cases):
     n, m = map(int, input().split())
     number_of_moves = 0
+    dynamic_list = {}
     first_line = [int(i) for i in input().split()]
     number_of_moves = max(first_line)
     current_tile = max(first_line)
-    lines = []
+    lines = [[]]
     for j in range(n - 1):
         new_line = [int(i) for i in input().split()]
         lines.append(new_line)
-    for k in range(n - 1):
-        new_moves, tile = get_number(k, current_tile)
-        number_of_moves += new_moves
-        current_tile = tile
+
+    number_of_moves += choose_best(n, 1, current_tile)
 
     print(number_of_moves)
+
+# Sample Input 0
+#
+# 1
+# 2 3
+# 3 1 2
+# 2 5 3
+# Sample Output 0
+#
+# 7
